f146e148c4f24478432a60ad5b789fac56ccbc5a
GitHub Action <action@github.com>
2024-02-05 04:18:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/storv/autologgerplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/storv/autologgerplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.storv.autologgerplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
